46cd01338923bbd2af49ba96859ef9d14c361dec
anuragsr <anurag.131092@gmail.com>
2024-07-05 13:00:26 +0530

    FC

---
+++ assets/js/plugins/parallax-slider.js
@@ -0,0 +1,27 @@
+var ParallaxSlider = function () {
+
+    return {
+        
+        //Parallax Slider
+        initParallaxSlider: function () {
+			$('#da-slider').cslider({
+			    current     : 0,    
+			    // index of current slide
+			     
+			    bgincrement : 30,  
+			    
+			    bgYpos : 30,  
+			    // increment the background position 
+			    // (parallax effect) when sliding
+			     
+			    autoplay    : false,
+			    // slideshow on / off
+			     
+			    interval    : 4000  
+			    // time between transitions
+			});
+        },
+
+    };
+
+}();        
